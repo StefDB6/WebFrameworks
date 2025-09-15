88f94982ce66b17a888d9fa2a300534770621403
Stef De Bie <stef.debie@student.ap.be>
2025-09-15 14:23:15 +0200

    some changes to first app

--- week1/first-react-project/src/App.tsx
+++ week1/first-react-project/src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css"
 
+// const App = () => {
+
+// }
+
+const getGreeting = () => {
+  return "Hallo iedereen!"
+}
+
+const reactLogo = "./assets/react.svg"
 function App() {
-  const [count, setCount] = useState(0)
+  const title : string = "World"
+  const random: number = Math.random()
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="hello">
+        <h1>Hello {title.toUpperCase()}! - world is een const</h1>
+
+        {random > 0.5 ? <p>GEWONNEN! {random}</p> : <p>verloren :( {random}</p>} 
+        <p>Hieronder alleen als je wint</p>
+        {random > 0.5 && <p>GEWONNEN! {random}</p>} 
+
+        <img src="/vite.svg" alt="vite logo" />
+        <img src={reactLogo} alt="react logo" />
+        <p>Dit is een function call: {getGreeting()}</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
 
-export default App
+export default App;
