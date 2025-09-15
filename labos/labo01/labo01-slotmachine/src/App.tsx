99ccb1624355acad5e144d42e4ccc209896e169b
Stef De Bie <stef.debie@student.ap.be>
2025-09-15 15:49:22 +0200

    minor tweaks

--- labos/labo01/labo01-slotmachine/src/App.tsx
+++ labos/labo01/labo01-slotmachine/src/App.tsx
@@ -4,6 +4,7 @@ const App = () => {
     const slot1 : number= Math.floor(Math.random() * 5)
     const slot2 : number= Math.floor(Math.random() * 5)
     const slot3 : number= Math.floor(Math.random() * 5)
+    
     const images : string[] = [
       "https://similonap.github.io/webframeworks-cursus/assets/images/slot-cherry-cce8464b32439efb4f79fba017477125.png",
       "https://similonap.github.io/webframeworks-cursus/assets/images/slot-lemon-52cd7112e8b6d398f97e69f8ce2da623.png",
@@ -11,6 +12,8 @@ const App = () => {
       "https://similonap.github.io/webframeworks-cursus/assets/images/slot-prune-e486170eb7c22e6e9aed5de0316b5209.png",
       "https://similonap.github.io/webframeworks-cursus/assets/images/slot-seven-e71e0d10655b5491197925624b5ac139.png"
     ]
+
+    const hasWon : boolean = slot1 === slot2 && slot2 === slot3;
     return (
         <>
             <h1>Labo 1 : Slots</h1>
@@ -26,7 +29,7 @@ const App = () => {
             <img src={images[4]} alt="" /> 
             */}
 
-            {slot1 === slot2 && slot2 === slot3 ? <p>Je hebt gewonnen!</p> : <p>Je bent verloren.</p>}
+            { hasWon ? <p>Je hebt gewonnen!</p> : <p>Je bent verloren.</p>}
 
             <div>
               <img src={images[slot1]} alt="" />
