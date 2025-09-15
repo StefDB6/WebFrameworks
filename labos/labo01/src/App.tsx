7abc7af1f05aa6ab97bb08310177cbbb58423da8
Stef De Bie <stef.debie@student.ap.be>
2025-09-15 14:34:46 +0200

    labo01: Expressions done

---
+++ labos/labo01/src/App.tsx
@@ -0,0 +1,28 @@
+
+const random : number = Math.random();
+const number1: number = Math.floor(Math.random() * 10)
+const number2: number = Math.floor(Math.random() * 10)
+
+const add = (a:number, b:number) => {
+  return a + b;
+}
+
+const multiply = (a:number, b:number) => {
+  return a * b;
+}
+
+const App = () => {
+    return (
+        <>
+            <h1>Labo 1</h1>
+
+            <p>{random}</p>
+            <p>{number1}</p>
+            <p>{number2}</p>
+
+            {random > 0.5 ? <p>{number1} * {number2} : {multiply(number1, number2)}</p> : <p>{number1} +   {number2} : {add(number1, number2)}</p>} 
+        </>
+    );
+}
+
+export default App;
