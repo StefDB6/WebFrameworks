8db515c2cdd373348c9184937de61a5397ce6771
Stef De Bie <stef.debie@student.ap.be>
2025-09-29 14:21:27 +0200

    completed labo04-statejoske

--- labos/labo04/labo04-statejoske/src/App.tsx
+++ labos/labo04/labo04-statejoske/src/App.tsx
@@ -4,14 +4,24 @@ import "./App.css";
 const ColorSquare = () => {
   const [color, setColor] = useState<number>(0)
   const colorArr = ["red", "blue", "green"]
-  let emotion = ":(";
-  let name = "Joske";
+  const [name, setName] = useState<string>("Joske")
+  const [emotion, setEmotion] = useState<string>(":(")
+  const [shape, setShape] = useState<string>("square")
   return (
-    <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+    <div style={{display: "flex", flexDirection: "column", alignItems: "center", borderRadius: 0}}>
       <p><b>{name}</b></p>
-      <div className="square" style={{backgroundColor: colorArr[color]}} onClick={() => color < 2 ? setColor(color + 1) : setColor(0)}>
+      <div className={shape} style={{backgroundColor: colorArr[color]}} onClick={() => color < 2 ? setColor(color + 1) : setColor(0)}>
         <p>{emotion}</p>
       </div>
+
+      <input type="text" value={name} onChange={(e) => setName(e.target.value)}/>
+      <button onClick={() => setName("Joske")}>Reset</button>
+      <select onChange={(e) => setEmotion(e.target.value)}>
+        <option value=":)">:)</option>
+        <option value=":|">:|</option>
+        <option value=":(">:(</option>
+      </select>
+      <input type="checkbox" onChange={() => shape === "square" ? setShape("circle") : setShape("square")}/>
     </div>
   )
 }
