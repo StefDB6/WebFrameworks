2aeec1e161da25a73c8eb9f1d8c69b042594c9aa
Stef De Bie <ssstef.de.bie@gmail.com>
2025-11-28 12:52:49 +0100

    first nextjs demo

---
+++ Next.js/week9/first-nextjs-app/src/app/oefening1/page.tsx
@@ -0,0 +1,11 @@
+
+const Oefening1Page = () => {
+
+    return(
+        <div>
+            <h1>Oefening 1</h1>
+        </div>
+    )
+}
+
+export default Oefening1Page
---
+++ Next.js/week9/first-nextjs-app/src/app/oefening2/page.tsx
@@ -0,0 +1,17 @@
+"use client"
+import { useEffect } from "react"
+
+const Oefening1Page = () => {
+
+    useEffect(() => {
+        fetch("")
+    }, [])
+
+    return(
+        <div>
+            <h1>Oefening 2</h1>
+        </div>
+    )
+}
+
+export default Oefening1Page
